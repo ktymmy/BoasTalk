d6d5178823ad73050de015a172c054f3fe35136a
ktymmy <2220154@ecc.ac.jp>
2023-11-05 00:54:11 +0900

    navibarのデザイン

---
+++ lib/constant/color_Const.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorConst {
+  static const Color main = Color.fromRGBO(168, 223, 142, 1);
+  static const Color base = Color.fromRGBO(243, 253, 232, 1);
+  static const Color accent = Color.fromRGBO(255, 191, 191, 1);
+
+  static const Color icon = Color.fromRGBO(49, 152, 0, 1);
+  static const Color unselecticon = Colors.white;
+}
