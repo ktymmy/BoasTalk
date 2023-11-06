6a57888a2b24a3f3fd59c9f2ec90e2e78be843f0
Alice-2525 <2220048@ecc.ac.jp>
2023-11-06 16:34:21 +0900

    add(color,mypageLayout)

---
+++ lib/constant/color_Const.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorConst {
+  static const Color background = Color(0xFFF3FDE8);
+  static const Color icon = Color(0xFFA8DF8E);
+  static const Color pink = Color(0xFFFFBFBF);
+}
