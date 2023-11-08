cdb32d53555203553a2b81a19db68958bd969eec
2220053 <2220053@ecc.ac.jp>
2023-11-09 01:03:47 +0900

    コミットします

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
 //page
 import './view/navibar.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
