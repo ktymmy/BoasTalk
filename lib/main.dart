bbf8e6071fde13f2cf6445c15e2c27adc1c88cba
ktymmy <106148367+ktymmy@users.noreply.github.com>
2023-11-09 10:15:45 +0900

    Revert "AppBarの作成"

--- lib/main.dart
+++ lib/main.dart
@@ -15,10 +15,13 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+
+      // home: _isLoggedIn == true ? Navigation() : LoginPage(),
       home: Navigation(),
     );
   }
