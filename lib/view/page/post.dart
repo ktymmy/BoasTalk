540335c9a073e719e24c262d346818d938dc0783
ryuto0024 <2220288@ecc.ac.jp>
2023-11-28 09:54:24 +0900

    [add] 画像関係作成

--- lib/view/page/post.dart
+++ lib/view/page/post.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:boastalk/constant/color_Const.dart';
 import '../navibar.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Post extends StatefulWidget {
   const Post({super.key});
@@ -11,6 +13,8 @@ class Post extends StatefulWidget {
 
 class _PostState extends State<Post> {
   bool _visible = false;
+  XFile? _image;
+  final imagePicker = ImagePicker();
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +34,7 @@ class _PostState extends State<Post> {
                     Navigator.push(
                       context,
                       MaterialPageRoute(builder: (context) => (Navigation())),
+                      //TODO
                     );
                   },
                   icon: Icon(
@@ -53,10 +58,7 @@ class _PostState extends State<Post> {
         body: SingleChildScrollView(
             child: Container(
           child: Column(children: [
-            Visibility(
-              visible: _visible,
-              child: _Image(),
-            ),
+            _Image(),
             _Text(),
             _Icons(),
           ]),
@@ -77,17 +79,17 @@ class _PostState extends State<Post> {
         ),
       ),
       margin: EdgeInsets.fromLTRB(20, 20, 10, 5), // 余白
-      child: const Padding(
+      child: Padding(
         padding: EdgeInsets.all(10.0), // 余白
         child: SizedBox(
           height: 250,
-          width: 325,
-          child: TextField(
-            keyboardType: TextInputType.multiline, // 複数行入力できるようにする
-            maxLines: null,
-            decoration:
-                InputDecoration(border: InputBorder.none, hintText: 'テキストを入力'),
-          ),
+          //   width: 325,
+          child: _image == null
+              ? Text(
+                  '写真を選択してください',
+                  style: Theme.of(context).textTheme.headline4,
+                )
+              : Image.file(File(_image!.path)),
         ),
       ),
     );
@@ -131,26 +133,33 @@ class _PostState extends State<Post> {
               showDialog(
                 context: context,
                 builder: (childContext) {
-                  return SimpleDialog(
+                  return AlertDialog(
                     backgroundColor: ColorConst.base,
                     title: Text(
-                        textAlign: TextAlign.center, "写真のアップロード方法を\n選択してください"),
+                      "写真のアップロード方法を\n選択してください",
+                      textAlign: TextAlign.center,
+                    ),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.all(Radius.circular(20)),
                     ),
-                    children: <Widget>[
-                      SimpleDialogOption(
-                        onPressed: () {
-                          Navigator.pop(childContext);
-                        },
-                        child: Container(
-                          margin: EdgeInsets.only(left: 40, right: 40),
-                          decoration: BoxDecoration(
-                            border:
-                                Border.all(color: ColorConst.main, width: 5),
-                            borderRadius: BorderRadius.circular(15.0),
+
+                    contentPadding: EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10), // 必要に応じてパディングを調整
+
+                    content: Column(
+                      children: <Widget>[
+                        OutlinedButton(
+                          onPressed: () {
+                            Navigator.pop(childContext);
+                            getImageFromCamera(); // 画像を取得する関数を呼び出す
+                          },
+                          style: OutlinedButton.styleFrom(
+                            side: BorderSide(color: ColorConst.main, width: 5),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                            ),
                           ),
-                          child: const Row(
+                          child: Row(
                             children: [
                               Icon(
                                 Icons.photo_camera,
@@ -165,19 +174,18 @@ class _PostState extends State<Post> {
                             ],
                           ),
                         ),
-                      ),
-                      SimpleDialogOption(
-                        onPressed: () {
-                          Navigator.pop(childContext);
-                        },
-                        child: Container(
-                          margin: EdgeInsets.only(left: 40, right: 40),
-                          decoration: BoxDecoration(
-                            border:
-                                Border.all(color: ColorConst.main, width: 5),
-                            borderRadius: BorderRadius.circular(15.0),
+                        OutlinedButton(
+                          onPressed: () {
+                            Navigator.pop(childContext);
+                            getImageFromGarally(); // 画像を取得する関数を呼び出す
+                          },
+                          style: OutlinedButton.styleFrom(
+                            side: BorderSide(color: ColorConst.main, width: 5),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                            ),
                           ),
-                          child: const Row(
+                          child: Row(
                             children: [
                               Icon(
                                 Icons.add_photo_alternate,
@@ -192,8 +200,8 @@ class _PostState extends State<Post> {
                             ],
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   );
                 },
               );
@@ -218,5 +226,23 @@ class _PostState extends State<Post> {
         ),
       ],
     );
+  }
+
+  Future getImageFromGarally() async {
+    final pickedFile = await imagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedFile != null) {
+        _image = XFile(pickedFile.path);
+      }
+    });
+  }
+
+  Future getImageFromCamera() async {
+    final pickedFile = await imagePicker.pickImage(source: ImageSource.camera);
+    setState(() {
+      if (pickedFile != null) {
+        _image = XFile(pickedFile.path);
+      }
+    });
   }
 }
